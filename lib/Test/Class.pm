ac1c0123872bc397da5c4a60441ede0fdd883aaf
Karen Etheridge <ether@cpan.org>
2014-07-05 05:12:03 +0000

    Import of ETHER/Test-Class-0.44 from CPAN.
    gitpan-cpan-distribution: Test-Class
    gitpan-cpan-version:      0.44
    gitpan-cpan-path:         ETHER/Test-Class-0.44.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/Test/Class.pm
+++ lib/Test/Class.pm
@@ -11,7 +11,7 @@ use Storable qw(dclone);
 use Test::Builder;
 use Test::Class::MethodInfo;
 
-our $VERSION = '0.43';
+our $VERSION = '0.44';
 
 my $Check_block_has_run;
 {
@@ -391,6 +391,7 @@ sub runtests {
                         _show_header($t, @tests) unless _has_no_tests($t, $method);
                         $all_passed = 0 unless _run_method($t, $method, \@tests);
                         if ( _threw_exception( $t, $method ) ) {
+			    next if ($method eq $test);
                             my $num_to_skip = _total_num_tests($t, @methods_to_run);
                             $Builder->skip( "$method died" ) for ( 1 .. $num_to_skip );
                             last;
